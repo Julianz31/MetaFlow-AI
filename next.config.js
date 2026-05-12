407528a43c66a3d06f1140d26c81431caa79f050
Julianz31 <julianzuluagaduque@gmail.com>
2026-05-11 19:26:06 -0500

    fix: use file:// font URIs for librsvg + include fonts in Vercel Lambda bundle

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    outputFileTracingIncludes: {
+      '/api/generate-image': ['./lib/fonts/**/*'],
+    },
+  },
 };
 
 module.exports = nextConfig;
